b549db92f8fcd0b56caf82225a2670f4fe5181c5
prince-kumar1504 <80267652+prince-kumar1504@users.noreply.github.com>
2023-08-24 07:32:58 +0530

    Time: 0 ms (100.00%), Space: 39.5 MB (30.18%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -4,12 +4,19 @@ class Solution {
             return 1;
         }
 
-        int[] dp = new int[n+1];
-        dp[0] = dp[1] = 1;
+//         int[] dp = new int[n+1];
+//         dp[0] = dp[1] = 1;
         
+        int prev = 1 ,prev2 = 1;
+         
         for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i-1] + dp[i-2];
+            
+            int cur =  prev + prev2;
+            
+            prev2 = prev;
+            prev = cur;
+            // dp[i] = dp[i-1] + dp[i-2];
         }
-        return dp[n];
+        return prev;
     }
 }
