0cdeb1b03794610a4df225b7525928091451c7eb
prince-kumar1504 <80267652+prince-kumar1504@users.noreply.github.com>
2023-09-19 04:24:38 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (22.52%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,26 +1,31 @@
-// Tabulation
+// space optimization
 
 class Solution {
     public int rob(int[] nums) {
         
         int n = nums.length;
         
-        int [] dp = new int [n];
-        Arrays.fill(dp,-1);
+        // int [] dp = new int [n];
+        // Arrays.fill(dp,-1);
         
-        dp[0] = nums[0];
+        int prev = nums[0];
+        int prev2 = 0;
         for ( int i = 1 ; i < n ; i++){
             
             int pick = nums[i];
             
-            if(i > 1) pick = pick + dp[i-2];
+            if(i > 1) pick = pick + prev2;
             
-            int notPick = dp[i-1];
+            int notPick = prev;
             
-            dp[i] = Math.max(pick,notPick);
+            int curi = Math.max(pick,notPick);
+            
+            
+            prev2 = prev;
+            prev = curi;            
                 
         }
-        return dp[n-1];
+        return prev;
     }
     
 }
